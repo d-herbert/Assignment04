a0d5e98248959536896193f0f1602af16902419d
d-herbert <deidre.m.herbert@gmail.com>
2022-03-12 22:29:06 -0800

    finished calc module

--- js/03-basic-calculator.js
+++ js/03-basic-calculator.js
@@ -1,6 +1,43 @@
 // IMPORT THE MODULE
+import * as calc from "./modules/calculator.js"
 
 // COLLECT NUMBER 1, NUMBER 2, AND OPERATION FROM THE USER
+let num1
+let num2
+let op
+let val
+
+function promptUser() {
+    num1 = parseFloat(prompt('enter a number'))
+    num2 = parseFloat(prompt('enter another number'))
+    op = prompt('would you like to add, subtract, multiply, or divide?')
+    return op, num1, num2
+}
 
 // CHECK TO SEE WHAT OPERATION THEY'RE USING
 // CALL THE APPROPRIATE FUNCTION
+function performOperation(op, num1, num2) {
+    if (op == 'add') {
+        val = calc.add(num1, num2)
+    }
+    else if (op == 'subtract') {
+        val = calc.sub(num1, num2)
+    }
+    else if (op == 'multiply') {
+        val = calc.mult(num1, num2)
+    }
+    else if (op == 'divide') {
+        val = calc.div(num1, num2)
+    }
+    else {
+        val = 'try again. you must enter one of the following options: add, subtract, multiply, or divide'
+    }
+    alert(val)
+}
+
+function main() {
+    op, num1, num2 = promptUser()
+    performOperation(op, num1, num2)
+}
+
+main()
--- js/modules/calculator.js
+++ js/modules/calculator.js
@@ -1,13 +1,22 @@
 // ADD A PRIVATE FUNCTION CALLED CALCULATE
+let privStr = 'calculator module has loaded'
+function calculate() {
+    console.log(privStr)
+}
+calculate()
 
 // ADD FOUR PUBLIC FUNCTIONS BELOW
-
 // ADD FUNCTION
+const add = (x, y) => x + y 
 
 // SUBTRACT FUNCTION
+const sub = (x, y) => x - y
 
 // MULTIPLY FUNCTION
+const mult = (x, y) => x * y
 
 // DIVIDE FUNCTION
+const div = (x, y) => x / y
 
 // EXPORT THE FOUR PUBLIC FUNCTIONS
+export {add, sub, mult, div}
